f7de3a228a86f94344f36a146f87c6a860e08e8f
Leodanis Zamora Cabrales <leomax@nauta.cu>
2019-05-28 16:07:25 +0200

    Prueba para verificar la extracción del mensaje
     On branch 1-agregar-a-eprhindingandauthentication-capacidad-para-coultar-un-mensaje
     Changes to be committed:
    	modified:   dhdicom/tests/test_hidding.py

--- dhdicom/tests/test_hidding.py
+++ dhdicom/tests/test_hidding.py
@@ -28,3 +28,11 @@ class PropertiesTest(unittest.TestCase):
         authentic, *l = self.hider.authenticate(data)
         self.assertFalse(authentic)
         self.assertListEqual(l[0], [0, 15])
+
+    def test_get_msg(self):
+        data = np.random.uniform(
+            low=0, high=65537, size=(128, 128)).astype(int)
+        msg = "Anier Soria Lorente"
+        self.hider.process(data, msg)
+        msg_extracted = self.hider.get_msg(data)
+        self.assertEqual(msg, msg_extracted)
